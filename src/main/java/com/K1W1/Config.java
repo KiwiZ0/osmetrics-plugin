da911fedba501d505dd7e23b60e0f1ee3b2e9863
Kiwi <9370847+KiwiZ0@users.noreply.github.com>
2025-07-16 18:34:44 -0400

    Update PluginName

--- src/main/java/com/K1W1/Config.java
+++ src/main/java/com/K1W1/Config.java
@@ -1,11 +1,10 @@
-package com.example;
+package com.K1W1;
 
-import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface Config extends net.runelite.client.config.Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/K1W1/OSMetricsTest.java
+++ src/test/java/com/K1W1/OSMetricsTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.K1W1;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OSMetricsTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OSMetrics.class);
 		RuneLite.main(args);
 	}
 }
